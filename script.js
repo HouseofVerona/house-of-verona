bed2d3a1413af3afd7838878ee70e6549fcf4c95
HouseofVerona <beast.rd.in@gmail.com>
2026-06-19 20:26:45 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 const menuContainer = document.getElementById("menu-container");
 
+const cartItemsContainer = document.getElementById("cart-items");
+
+const totalElement = document.getElementById("cart-total");
+
+let cart = [];
+
 fetch("data/menu.json")
 .then(response => response.json())
 .then(products => {
@@ -17,8 +23,49 @@ card.innerHTML = `
 <button>Add To Cart</button>
 `;
 
+const button = card.querySelector("button");
+
+button.addEventListener("click", () => {
+addToCart(product);
+});
+
 menuContainer.appendChild(card);
 
 });
 
 });
+
+function addToCart(product){
+
+cart.push(product);
+
+renderCart();
+
+}
+
+function renderCart(){
+
+cartItemsContainer.innerHTML = "";
+
+let total = 0;
+
+cart.forEach(item => {
+
+total += item.price;
+
+const div = document.createElement("div");
+
+div.classList.add("cart-item");
+
+div.innerHTML = `
+<span>${item.name}</span>
+<span>₹${item.price}</span>
+`;
+
+cartItemsContainer.appendChild(div);
+
+});
+
+totalElement.textContent = total;
+
+}
